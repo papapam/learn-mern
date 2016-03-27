ce1ada102b01acba40f5e1a1f76133d2cd5d5f8a
papapam <plam_3cheeza@hotmail.com>
2016-03-27 13:02:21 +0700

    edit ip server

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://54.200.57.125:27017/mern',
   port: process.env.PORT || 8000,
 };
 
